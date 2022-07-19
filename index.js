6a45c1c8b184159f8da4e92f1b0b347f18c3ab9e
Michael Franco <franco.luis.michael@gmail.com>
2022-07-18 19:03:45 -0500

    commit tree fe2e51839543c82ba96e2ce4ecbd04f32afe5bd2

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+   return(`This Saturday, I want to ${activity}!`);
+
+}
+
+function mondayWork(activity="go to the office"){
+    return(`This Monday, I will ${activity}.`)
+
+}
+
+
+// function wrapAdjective(a="*") {
+//     console.log(a)
+//     const inner = function(a) {
+//         return `You are ${a}${a}!`
+//     }
+//     return inner
+// }
+
+function wrapAdjective(work="*") {
+    return function innerFunction(part1 = "special") {
+        return (`You are ${work}${part1}${work}!`)
+    }
+        
+}
+wrapAdjective();
